0d7ce953cc66c924b4d8bbe899793e85748c37a7
aravind-kumar <karavindkumar1993@gmail.com>
2018-01-07 21:07:48 -0500

    Adding variables

---
+++ variables.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+  var helloWorld string = "Hello World"
+  fmt.Println(helloWorld)
+  
+  var a,b int = 2,3
+  fmt.Println("Sum of the values is ", a+b)
+   
+  var str1,str2 string = "aravind"," kumar"
+
+  fmt.Println("My name is ", str1 + str2)
+   
+   c,d:=5,6
+   
+   fmt.Println("The new sum is " , c+d) 
+  
+}
