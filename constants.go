25f509dce0f519553c067273c72203443f99861d
aravind-kumar <karavindkumar1993@gmail.com>
2018-01-07 21:18:54 -0500

    Constants

---
+++ constants.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+import "math"
+const name = "Aravind"
+
+const endName = "Kumar"
+//Constant are declared using const
+
+func main() {
+	fmt.Println("My name is ", name+endName)
+	fmt.Println(math.Pi)
+
+}
