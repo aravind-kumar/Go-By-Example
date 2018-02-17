850ad6ca3174ef1111258b1ed56847439fe247f8
aravind-kumar <karavindkumar1993@gmail.com>
2018-02-17 15:22:41 -0500

    Accpeted code for reverse ll in iterative

---
+++ reverse_LL_iterative.go
@@ -0,0 +1,26 @@
+package main
+
+
+type Node struct {
+    val int
+    Next *Node
+}
+
+func reverseLL(head *Node) *Node {
+     
+     var pre *Node = nil
+     
+     for head != nil {
+         var newHead = head.Next
+         head.Next = pre
+         pre = head
+         head = newHead
+     }
+     return head
+}
+
+
+func main() {
+
+
+}
