b798d6f1d5b844bf01cdf3906559eb3b3ea8073a
aravind-kumar <karavindkumar1993@gmail.com>
2018-02-28 23:51:36 -0500

    Accepted code for plus one

---
+++ plus_one.go
@@ -0,0 +1,24 @@
+package main
+import fmt
+
+func plusOne(digits []int) []int {
+    var n = len(digits)
+    for i:=n-1;i>=0;i-=1 {
+        if digits[i] < 9 {
+            digits[i]+=1
+            return digits
+        }
+        digits[i]=0
+    }
+    newArray := make([]int,n+1,n+1)
+    newArray[0] = 1
+    return newArray
+    
+}
+
+func main() {
+
+   
+
+
+}
