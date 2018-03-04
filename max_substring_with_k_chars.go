45bab9e583a593e437d8513f45d596cadc75ca8e
aravind-kumar <karavindkumar1993@gmail.com>
2018-03-04 14:15:46 -0500

    Max substring with k chars go

---
+++ max_substring_with_k_chars.go
@@ -0,0 +1,54 @@
+package main
+import "fmt"
+
+func max(a,b int) int {
+    if a > b {
+        return a
+    } else {
+        return b
+    }
+}
+
+func lengthOfLongestSubstringKDistinct(s string, k int) int {
+    sMap := make(map[byte]int,len(s))
+
+    start,end,targetCount,maxLen := 0,0,0,math.MinInt32
+    
+    for end<len(s) {
+         
+        if sMap[s[end]] == 0 {
+            targetCount+=1
+        }
+        
+        sMap[s[end]]+=1
+        
+        for targetCount > k && start<len(s) {
+
+            sMap[s[start]]-=1
+            
+            if sMap[s[start]] == 0 {
+                targetCount -= 1
+            }
+            start+=1
+            
+        }
+        
+        maxLen = max(maxLen,end-start+1)
+        
+        end+=1
+    }
+    
+    if maxLen == math.MinInt32 {
+        return 0
+    } else {
+        return maxLen
+    }
+}
+
+
+func main() {
+
+
+
+
+}
